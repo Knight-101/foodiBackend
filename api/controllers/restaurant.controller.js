842154824a92b749073694a9840e0f3ebb69f910
Sanskar Mohapatra <sanskarmohapatra@gmail.com>
2021-08-25 00:59:47 +0530

    backend final

--- api/controllers/restaurant.controller.js
+++ api/controllers/restaurant.controller.js
@@ -33,38 +33,3 @@ exports.restByName = async (req, res, next) => {
     next();
   }
 };
-
-// exports.rateFilter = async (req, res, next) => {
-//   try {
-//     await Hotel.find({ Rating: req.params.rating })
-//       .then((hotels) => {
-//         res.status(200).json(hotels);
-//       })
-//       .catch((err) => {
-//         res.status(404).json({ message: err });
-//       });
-
-//     //   await user.updateOne(newUserObject, { override: true, upsert: true });
-//     //   const savedUser = await User.findById(user._id);
-//   } catch (error) {
-//     console.log(error);
-//     next();
-//   }
-// };
-
-// exports.book = async (req, res) => {
-//   await User.findOne({ email: req.userEmail }, (err, foundUser) => {
-//     try {
-//       if (foundUser) {
-//         const newBook = req.body;
-//         foundUser.bookings.push(newBook);
-//         foundUser.save();
-//         res.json({ ok: 1 });
-//       } else {
-//         res.send("User Not Found");
-//       }
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   });
-// };
